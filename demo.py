42fd1557cfac9a72428b4b14385fb03f49fedd38
ssubhalaxmi28 <ssubhalaxmi28j@gmail.com>
2025-04-06 01:10:09 +0530

    EDA added

--- demo.py
+++ demo.py
@@ -1,3 +1,10 @@
 from us_visa.logger import logging
+from us_visa.exception import USvisaException
+import sys #system package
 
-logging.info("welcome")
+# logging.info("welcome")
+try:
+    a=1/"10"
+except Exception as e:
+    logging.info(e)
+    raise USvisaException(e, sys) from e
